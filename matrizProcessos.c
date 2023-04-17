72f69cca91ba1dd974e9e35b00e580d51affd4fa
JoseLucioS <dragaolucio@gmail.com>
2023-04-17 19:30:40 -0300

    add-> programas iniciais para iniciar implementacoes

---
+++ matrizProcessos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Esse programa realiza a multiplicacao das matrizes atraves de processos*/
+
+
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
---
+++ matrizSequencial.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Esse programa realiza a multiplicacao das matrizes de forma sequencial*/
+
+
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
